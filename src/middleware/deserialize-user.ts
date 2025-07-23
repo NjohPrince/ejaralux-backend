02de99ed59945b30c7ec2def9fa1f9f684aba172
NjohPrince <jufredprince@gmail.com>
2025-07-23 05:30:14 +0100

    fix(auth): fixed issue with token expiry

--- src/middleware/deserialize-user.ts
+++ src/middleware/deserialize-user.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import { TokenExpiredError } from "jsonwebtoken";
 
 import { findUserById } from "../services/user.service";
 import AppError from "../utils/app-error.util";
@@ -26,25 +27,44 @@ export const deserializeUser = async (
       return next(new AppError(401, "You are not logged in"));
     }
 
-    const decoded = verifyJwt<{ sub: string }>(
-      access_token,
-      "accessTokenPublicKey"
-    );
-
-    if (!decoded) {
-      return next(new AppError(401, `Invalid token or user doesn't exist`));
+    let decoded;
+    try {
+      decoded = verifyJwt<{ sub: string }>(
+        access_token,
+        "accessTokenPublicKey"
+      );
+    } catch (error) {
+      if (error instanceof TokenExpiredError) {
+        return next(
+          new AppError(401, "Access token expired", "ACCESS_TOKEN_EXPIRED")
+        );
+      } else {
+        return next(new AppError(401, "Invalid access token"));
+      }
     }
 
-    const session = await redisClient.get(decoded.sub);
+    const session = await redisClient.get(decoded?.sub || "");
 
     if (!session) {
-      return next(new AppError(401, `Invalid token or session has expired`));
+      return next(
+        new AppError(
+          401,
+          `Invalid token or session has expired`,
+          "ACCESS_TOKEN_EXPIRED"
+        )
+      );
     }
 
     const user = await findUserById(JSON.parse(session).id);
 
     if (!user) {
-      return next(new AppError(401, `Invalid token or session has expired`));
+      return next(
+        new AppError(
+          401,
+          `Invalid token or session has expired`,
+          "ACCESS_TOKEN_EXPIRED"
+        )
+      );
     }
 
     res.locals.user = user;
--- src/utils/app-error.util.ts
+++ src/utils/app-error.util.ts
@@ -1,10 +1,16 @@
 export default class AppError extends Error {
   status: string;
   isOperational: boolean;
-  constructor(public statusCode: number = 500, public message: string) {
+  code?: string;
+  constructor(
+    public statusCode: number = 500,
+    public message: string,
+    code?: string
+  ) {
     super(message);
     this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
     this.isOperational = true;
+    if (code) this.code = code;
 
     Error.captureStackTrace(this, this.constructor);
   }
